2b918cf4c9a4c878c2e7c6d29f2ab4e0eadda745
darshik <darshik163.rejoice@gmail.com>
2022-12-23 14:35:03 +0530

    initial setup

---
+++ dist/tailwind.config.dev.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+      visby: ["Visby", "system-ui"]
+    },
+    extend: {
+      colors: {
+        green: {
+          1: "#004aad" // 1: "#004aad",
+
+        }
+      }
+    }
+  },
+  plugins: []
+};
+//# sourceMappingURL=tailwind.config.dev.js.map
--- src/app/pages/DashboardPage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import {
-  Dashboard
-} from "../../_metronic/_partials";
-
-export default function DashboardPage() {
-  return <Dashboard />;
-}
